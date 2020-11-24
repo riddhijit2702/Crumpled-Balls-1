0d36e3cd423a505b86ea461f023b46de7242bbda
riddhijit2702 <74096278+riddhijit2702@users.noreply.github.com>
2020-11-24 12:10:51 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -6,8 +6,16 @@ class Dustbin{
         isStatic:true,
         
     }
-    this.body=Bodies.rectangle(x,y,50,100,options)
-    World.add(this.body,World)
+    this.width=width;
+    this.height=height;
+    this.body=Bodies.rectangle(x,y+height/2,width,height,options)
+    World.add(world,this.body)
+
+    this.rightbody=Bodies.rectangle(x+width/2,y-height/2,height,width,options)
+    World.add(world,this.rightbody)
+
+    this.leftbody=Bodies.rectangle(x-width/2,y-height/2,height,width,options)
+    World.add(world,this.leftbody)
 /*this.x=x;
 this.y=y;
 this.angle=angle*/
@@ -16,14 +24,29 @@ this.angle=angle*/
 display(){
 
 var pos =  this.body.position
-var angle = this.body.angle
+var pos2 =  this.rightbody.position
+var pos3 =  this.leftbody.position
+//var angle = this.body.angle
 
 push()
 translate(pos.x,pos.y)
-rotate(angle)
 strokeWeight(2)
 fill("white")
-rect(0,300,30,150)
+rect(0,0,this.width,this.height)
+pop();
+
+push()
+translate(pos2.x,pos2.y)
+strokeWeight(2)
+fill("white")
+rect(0,0,this.height,this.width)
+pop();
+
+push()
+translate(pos3.x,pos3.y)
+strokeWeight(2)
+fill("white")
+rect(0,0,this.height,this.width)
 pop();
 }
 
