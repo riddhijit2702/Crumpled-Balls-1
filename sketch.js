f99fe0b9eb30235f84bfec132f6e863aceddea22
riddhijit2702 <74096278+riddhijit2702@users.noreply.github.com>
2020-11-24 12:09:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,11 +2,8 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var ball,dustbin,ground,dustbin1,dustbin2;
-function preload()
-{
-	
-}
+var ball,dustbin,ground;
+
 
 function setup() {
 	createCanvas(800, 700);
@@ -18,9 +15,8 @@ function setup() {
 	//Create the Bodies Here.
    ball=new Ball(100,663,30)
   ground=new Ground(400,680,800,20)
-  dustbin=new Dustbin(400,300,30,150)
-  dustbin1=new Dustbin(600,300,30,150)
-  dustbin2=new Tilted(500,350,150,30)
+  dustbin=new Dustbin(600,600,200,20)
+ 
 	Engine.run(engine);
   
 }
@@ -34,15 +30,16 @@ function draw() {
   ground.display();
   ball.display();
   dustbin.display();
-  dustbin1.display();
-  dustbin2.display();
-  drawSprites();
+  
  
 }
 
 function keyPressed() {
 	if(keyCode===UP_ARROW){
-   Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85})
+   Matter.Body.applyForce(ball.body,ball.body.position,{x:15,y:-15})
 	}
 }
+
+
+
 
